76f5732792f1ea68bfeb67cff0cb7772ee20656c
Mastersai@16 <masstersaitej2000@gmail.com>
2023-07-25 13:21:02 +0530

    updated

--- convert.py
+++ convert.py
@@ -3,7 +3,7 @@ import pandas as pd
 class Numerical:
     def converted(self, column, value):
         df_train = pd.read_csv('numerical_data.csv')
-        cat_data = pd.read_csv(r'Dataset\loan_train.csv')
+        cat_data = pd.read_csv('loan_train.csv')
         
         # Check if the value exists in the column of the DataFrame
         if value in cat_data[column].values:
